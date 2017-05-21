b407f0cc09eca6e2c21f5542205736318ee235ac
Francesco Gonnella <francesco.gonnella@gmail.com>
2017-05-21 12:32:22 +0100

    Get last episode in crontab does not send email eny longer

--- conf.sh
+++ conf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /mnt/Seagate
-echo "*  *    * * *   pi      /usr/bin/perl /usr/local/bin/GetLastEpisode" >> /etc/crontab
+echo "*  *    * * *   pi      /usr/bin/perl /usr/local/bin/GetLastEpisode >/dev/null 2>&1" >> /etc/crontab
 echo "/dev/sda1 /mnt/Seagate  ext4 defaults,noatime 0 1" >> /etc/fstab
