f119fae2ddfccee2d06ea182836bfdd57da358df
Francesco Gonnella <francesco.gonnella@gmail.com>
2017-06-10 15:32:43 +0000

    Some file missing, some debug

--- copy.sh
+++ copy.sh
@@ -4,6 +4,7 @@ cp service/qbittorrent.service /etc/systemd/system
 cp service/lcd.service /etc/systemd/system
 
 cp conf/GetLastEpisode.txt /etc/
+cp conf/organise.txt /etc/
 cp conf/bashrc /home/pi/.bashrc
 cp conf/ssmtp.conf /etc/ssmtp/ssmtp.conf
 cp conf/smb.conf /etc/samba/
@@ -14,5 +15,6 @@ cp exec/GetLastEpisode /usr/local/bin
 cp exec/login.txt      /usr/local/bin
 cp exec/organise	      /usr/local/bin
 cp exec/resurrect_qbt  /usr/local/bin
+cp exec/lcd.py /usr/local/bin
 
 chmod +x /usr/local/bin/*
